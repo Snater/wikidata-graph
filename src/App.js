c9c69fb4c754f84396d26f3055855ec0773aae30
Snater <git@snater.com>
2019-03-13 20:31:48 +0100

    Refactored App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { QueryManagerContainer, ChartContainer, FormContainer } from './containers';
 import './App.css';
 
-class App extends Component {
-
-	/**
-	 * @inheritdoc
-	 */
-	render() {
-		return (
-			<div className="App">
-				<QueryManagerContainer />
-				<div className="App__form-container">
-					<FormContainer />
-				</div>
-				<ChartContainer />
-			</div>
-		);
-	}
-}
-
-export default App;
+export default () => (
+	<div className="App">
+		<QueryManagerContainer />
+		<div className="App__form-container">
+			<FormContainer />
+		</div>
+		<ChartContainer />
+	</div>
+);

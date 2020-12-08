cf6a003b3d5d19ae779389ad73e12f5df991a956
Snater <git@snater.com>
2020-12-08 20:57:47 +0100

    Converted ModeSelect to functional component

--- src/components/ModeSelect/ModeSelect.js
+++ src/components/ModeSelect/ModeSelect.js
@@ -1,51 +1,30 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
+import PropTypes from 'prop-types';
+import React from 'react';
 import Select from '@material-ui/core/Select';
 
-class ModeSelect extends Component {
+const OPTIONS = [
+	{value: 'forward', label: 'Forward'},
+	{value: 'reverse', label: 'Reverse'},
+	{value: 'both', label: 'Bidirectional'},
+];
 
-	/**
-	 * @type {Object[]}
-	 */
-	static options = [
-		{value: 'forward', label: 'Forward'},
-		{value: 'reverse', label: 'Reverse'},
-		{value: 'both', label: 'Bidirectional'},
-	];
-
-	/**
-	 * @inheritdoc
-	 */
-	render() {
-		return (
-			<FormControl margin="dense">
-				<InputLabel htmlFor={this.props.id}>Direction</InputLabel>
-				<Select
-					inputProps={{
-						id: this.props.id
-					}}
-					value={this.props.value}
-					onChange={e => this.props.onChange(e.target.value)}
-				>
-					{ModeSelect.options.map(option =>
-						<MenuItem
-							key={option.value}
-							value={option.value}
-						>{option.label}</MenuItem>
-					)}
-				</Select>
-			</FormControl>
-		);
-	}
+export default function ModeSelect({id, onChange, ...rest}) {
+	return (
+		<FormControl margin="dense">
+			<InputLabel htmlFor={id}>Direction</InputLabel>
+			<Select inputProps={{id}} onChange={e => onChange(e.target.value)} {...rest}>
+				{OPTIONS.map(option =>
+					<MenuItem key={option.value} value={option.value}>{option.label}</MenuItem>
+				)}
+			</Select>
+		</FormControl>
+	);
 }
 
 ModeSelect.propTypes = {
 	id: PropTypes.string.isRequired,
-	value: PropTypes.string,
-	onChange: PropTypes.func,
+	onChange: PropTypes.func.isRequired,
 };
-
-export default ModeSelect;

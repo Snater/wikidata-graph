a0d747e199940315f4667fbf5793147e5f3acc4c
Snater <git@snater.com>
2023-03-01 07:00:56 +0100

    Fixed createTheme import

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import './index.css';
 import 'roboto-fontface';
 import {ThemeProvider as MuiThemeProvider} from '@material-ui/core/styles';
 import {ThemeProvider} from 'styled-components';
-import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import createTheme from '@material-ui/core/styles/createTheme';
 
-const theme = createMuiTheme();
+const theme = createTheme();
 
 ReactDOM.render(
 	<MuiThemeProvider theme={theme}>

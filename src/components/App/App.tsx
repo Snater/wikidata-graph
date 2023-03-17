9fdd2a235899c0961e3cd4e925c1217f3af6ca67
Snater <git@snater.com>
2023-03-17 08:46:09 +0100

    Revisited App component

---
+++ src/components/App/App.tsx
@@ -0,0 +1,27 @@
+import Box from '@mui/material/Box';
+import Chart from '../Chart';
+import Form from '../Form';
+import {QueryContextProvider} from './QueryContext';
+import QueryManager from '../QueryManager';
+import React from 'react';
+import WikidataInterface from '../../lib/WikidataInterface';
+
+export default () => (
+	<Box display="flex" height={1} width={1}>
+		<QueryContextProvider>
+			<QueryManager/>
+			<Box
+				bgcolor="common.white"
+				boxShadow={8}
+				height={1}
+				maxHeight={1}
+				position="absolute"
+				sx={{
+					overflowY: 'auto',
+				}}>
+				<Form/>
+			</Box>
+			<Chart getEntityImage={WikidataInterface.getEntityImage}/>
+		</QueryContextProvider>
+	</Box>
+);

7897794e93169c14fd4a7edc2d873989034a27f0
Snater <git@snater.com>
2023-03-19 07:33:05 +0100

    Revisited WdqsButton

---
+++ src/components/WdqsButton/WdqsButton.tsx
@@ -0,0 +1,20 @@
+import React, {useCallback} from 'react';
+import Button, {ButtonProps} from '@mui/material/Button';
+import Query from '../../lib/Query';
+import SparqlGenerator from '../../lib/SparqlGenerator';
+
+type WdqsButtonProps = {
+	query: Query
+} & ButtonProps
+
+export default function WdqsButton({query, ...rest}: WdqsButtonProps) {
+
+	const handleClick = useCallback(() => {
+		const baseUrl = 'https://query.wikidata.org';
+		window.open(`${baseUrl}/#${encodeURIComponent(SparqlGenerator.generate(query))}`);
+	}, [query]);
+
+	return (
+		<Button color="primary" onClick={handleClick} variant="contained" {...rest}/>
+	);
+}

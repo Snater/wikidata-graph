46c7fd94d726809dce621b514e935b41ded5f2b1
Snater <git@snater.com>
2023-04-26 08:37:50 +0200

    Added return types to Vector

--- src/lib/Vector.ts
+++ src/lib/Vector.ts
@@ -5,23 +5,23 @@ interface Point {
 
 export default class Vector {
 
-	static size({x, y}: Point) {
+	static size({x, y}: Point): number {
 		return Math.sqrt(x * x + y * y);
 	}
 
-	static sum({x: x1, y: y1}: Point, {x: x2, y: y2}: Point) {
+	static sum({x: x1, y: y1}: Point, {x: x2, y: y2}: Point): Point {
 		return {x: x1 + x2, y: y1 + y2};
 	}
 
-	static diff({x: x1, y: y1}: Point, {x: x2, y: y2}: Point) {
+	static diff({x: x1, y: y1}: Point, {x: x2, y: y2}: Point): Point {
 		return {x: x1 - x2, y: y1 - y2};
 	}
 
-	static prod({x, y}: Point, scalar: number) {
+	static prod({x, y}: Point, scalar: number): Point {
 		return {x: x * scalar, y: y * scalar};
 	}
 
-	static scale(point: Point, scalar: number) {
+	static scale(point: Point, scalar: number): Point {
 		return Vector.prod(point, (scalar / Vector.size(point)));
 	}
 }

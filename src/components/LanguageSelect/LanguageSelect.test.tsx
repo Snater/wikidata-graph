fa1eb3a0596dfcdeda829483807e58a4f3f0de9c
Snater <git@snater.com>
2024-09-13 17:20:38 +0200

    Extended LanguageSelect tests

--- src/components/LanguageSelect/LanguageSelect.test.tsx
+++ src/components/LanguageSelect/LanguageSelect.test.tsx
@@ -11,26 +11,25 @@ beforeAll(() => {
 	user = userEvent.setup();
 });
 
-let getLanguagesSpy;
+let getLanguagesSpy: jest.SpyInstance;
 
 beforeEach(() => {
 	getLanguagesSpy = jest.spyOn(Wikidata, 'getLanguages');
-	getLanguagesSpy.mockImplementation(async () => [
-		{code: 'da', label: 'Dansk'},
-		{code: 'de', label: 'Deutsch'},
-		{code: 'en', label: 'English'},
-	]);
 });
 
 afterEach(() => {
 	getLanguagesSpy.mockRestore();
 });
 
-test('onChange prop', async () => {
+it('triggers onChange event', async () => {
+	getLanguagesSpy.mockImplementation(async () => [
+		{code: 'da', label: 'Dansk'},
+		{code: 'de', label: 'Deutsch'},
+		{code: 'en', label: 'English'},
+	]);
+
 	const handleChange = jest.fn();
-	const {getByRole, getByText} = render(
-		<LanguageSelect id="id" onChange={handleChange} value=""/>
-	);
+	const {getByRole, getByText} = render(<LanguageSelect id="id" onChange={handleChange} value=""/>);
 
 	await waitFor(() => expect(getByRole('combobox')).not.toBeDisabled());
 
@@ -39,4 +38,15 @@ test('onChange prop', async () => {
 
 	expect(handleChange).toHaveBeenCalledTimes(1);
 	expect(handleChange).toHaveBeenCalledWith('de');
+});
+
+it('remains empty when not languages are retrieved', async () => {
+	const mock = jest.fn(() => null);
+	getLanguagesSpy.mockImplementation(async () => mock());
+
+	const {getByRole} = render(<LanguageSelect id="id" native onChange={jest.fn} value=""/>);
+
+	expect(mock).toHaveBeenCalledTimes(1);
+
+	expect(getByRole('combobox')).toBeDisabled();
 });
